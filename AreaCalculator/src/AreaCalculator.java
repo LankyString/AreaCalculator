7063cb33324d0c3e2daa34bab9e0fef2a2a5ba71
Andrew <39748224+LankyString@users.noreply.github.com>
2019-03-30 09:23:21 +0000

    Add files via upload

---
+++ AreaCalculator/src/AreaCalculator.java
@@ -0,0 +1,20 @@
+public class AreaCalculator
+{
+    public static double area(double radius)
+    {
+        if(radius < 0 )
+        {
+            return -1;
+        }
+        double PI = Math.PI;
+        return radius * radius * PI;
+    }
+    public static double area(double x, double y)
+    {
+        if((x < 0) || (y < 0))
+        {
+            return -1;
+        }
+        return x * y;
+    }
+}
---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double a = AreaCalculator.area(42);
+        System.out.println(a);
+
+        double b = AreaCalculator.area(10, 3);
+        System.out.println(b);
+    }
+}
